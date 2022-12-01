37bacb85957d347fb384cce820b7d0bbc875b372
Uchenna <dp_beryl@yahoo.com>
2022-12-01 11:44:41 -0700

    Adding a LocalDev branch.

---
+++ main.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import math as mt
+import matplotlib.pyplot as plt
+import plotly.express as px
+from plotly.offline import plot
+import sklearn.tree as skTree
+import sklearn.metrics as skMetrics
+from sklearn.model_selection import train_test_split
+import plotly.graph_objects as go
+import pandas as pd
+import csv
+import matplotlib.pyplot as plt
+df = pd.read_csv("C:\Users\maria\OneDrive\00 Norquest College\Courses\ML Work Integrated 2\titanictrain.csv")
+df.head(5)  # Used to display top 5
+
+
+df.head()
